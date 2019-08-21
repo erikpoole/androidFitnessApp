746e5de646658607216b9aee72a21baf31fbdc2a
Erik Poole <Mahkumazahn@gmail.com>
2019-08-21 10:22:51 -0600

    Added button functionality

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.button1:
+                Toast toast = Toast.makeText(getApplicationContext(), "Button", Toast.LENGTH_SHORT);
+                toast.show();
+                break;
+        }
     }
 }
