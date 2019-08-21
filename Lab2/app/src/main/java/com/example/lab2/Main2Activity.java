c758aedfa6ed49eb2b2680279de5790e4b9ad983
Katie <katie.robinson@utah.edu>
2019-08-21 11:41:53 -0600

    Creating second click listener

--- Lab2/app/src/main/java/com/example/lab2/Main2Activity.java
+++ Lab2/app/src/main/java/com/example/lab2/Main2Activity.java
@@ -2,12 +2,22 @@ package com.example.lab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_take_picture:
+                Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+        }
     }
 }
