7507d71eeabca06ecd966cf28cb90c53d44f24f1
Katie <katie.robinson@utah.edu>
2019-08-21 11:55:58 -0600

    Camera stuff works

--- Lab2/app/src/main/java/com/example/lab2/Main2Activity.java
+++ Lab2/app/src/main/java/com/example/lab2/Main2Activity.java
@@ -1,23 +1,49 @@
 package com.example.lab2;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity implements View.OnClickListener{
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Button button2 = findViewById(R.id.button_take_picture);
+        button2.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button_take_picture:
-                Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"Case activated",Toast.LENGTH_SHORT).show();
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(cameraIntent.resolveActivity(getPackageManager())!=null){
+                    startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap thumbnailImage = (Bitmap) extras.get("data");
+            ImageView image = (ImageView) findViewById(R.id.image);
+            image.setImageBitmap(thumbnailImage);
         }
     }
 }
