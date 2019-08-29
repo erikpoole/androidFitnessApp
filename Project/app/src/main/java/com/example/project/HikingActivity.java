0157b0cb680e7d2d2f10051a2918b5fdbe5f81c4
Katie <katie.robinson@utah.edu>
2019-08-28 23:02:45 -0600

    Finished hiking activity, should completely work

--- Project/app/src/main/java/com/example/project/HikingActivity.java
+++ Project/app/src/main/java/com/example/project/HikingActivity.java
@@ -1,14 +1,17 @@
 package com.example.project;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,7 +21,7 @@ import java.util.Locale;
 
 public class HikingActivity extends AppCompatActivity {
 
-    final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 111;
+    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 111;
     private String latitude;
     private String longitude;
 
@@ -28,6 +31,8 @@ public class HikingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hiking);
 
         getLocation();
+
+        // Open the Google Maps app and search for nearby hiking trails
         Uri gmmIntentUri = Uri.parse("geo:" + latitude + "," + longitude + "-?q=hiking");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
@@ -37,20 +42,22 @@ public class HikingActivity extends AppCompatActivity {
     }
 
     public void getLocation() {
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setSpeedRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final Looper looper = null;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
         } else {
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            try {
-                latitude = String.format(Locale.US, "%.4f", location.getLatitude());
-                longitude = String.format(Locale.US, "%.4f", location.getLongitude());
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(getApplicationContext(), "No previous locations!", Toast.LENGTH_LONG).show();
-            }
+            locationManager.requestSingleUpdate(criteria, locationListener, looper);
         }
     }
 
@@ -58,22 +65,37 @@ public class HikingActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                    @SuppressLint("MissingPermission") Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                    try {
-                        latitude = String.format(Locale.US, "%.4f", location.getLatitude());
-                        longitude = String.format(Locale.US, "%.4f", location.getLongitude());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        Toast.makeText(getApplicationContext(), "No previous locations!", Toast.LENGTH_LONG).show();
-                    }
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocation();
                 } else {
                     Toast.makeText(getApplicationContext(), "GetFit() cannot access your location.", Toast.LENGTH_LONG).show();
                 }
             }
         }
     }
+
+    final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("Location Changes", location.toString());
+            latitude = String.format(Locale.US, "%.4f", location.getLatitude());
+            longitude = String.format(Locale.US, "%.4f", location.getLongitude());
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Log.d("Status Changed", String.valueOf(status));
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d("Provider Enabled", provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d("Provider Disabled", provider);
+        }
+    };
 
 }
