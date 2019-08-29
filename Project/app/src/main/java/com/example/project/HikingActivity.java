4c34f0e7240317ba04b112912575e14d28821a22
Katie <katie.robinson@utah.edu>
2019-08-28 20:26:27 -0600

    Opens hiking app, deleted all the crazy permissions stuff and am going to rewrite it more simply

--- Project/app/src/main/java/com/example/project/HikingActivity.java
+++ Project/app/src/main/java/com/example/project/HikingActivity.java
@@ -1,23 +1,14 @@
 package com.example.project;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class HikingActivity extends AppCompatActivity implements View.OnClickListener {
-
-    final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +21,14 @@ public class HikingActivity extends AppCompatActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-        Toast.makeText(getApplicationContext(), "Hiking!", Toast.LENGTH_LONG).show();
+//        String currentLocation = getLocation();
+//        if (currentLocation.isEmpty()) {
+//            Toast.makeText(getApplicationContext(), "Can't obtain location.", Toast.LENGTH_LONG).show();
+//            return;
+//        }
+//        Toast.makeText(getApplicationContext(), currentLocation, Toast.LENGTH_LONG).show();
 
-        String currentLocation = getLocation();
-        if (currentLocation.isEmpty()) {
-            Toast.makeText(getApplicationContext(), "Can't obtain location.", Toast.LENGTH_LONG).show();
-            return;
-        }
-        Toast.makeText(getApplicationContext(), currentLocation, Toast.LENGTH_LONG).show();
-
-        Uri gmmIntentUri = Uri.parse("google.streetview:cbll=46.414382,10.013988");
+        Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4192?q=" + Uri.encode("1st & Pike, Seattle"));
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
