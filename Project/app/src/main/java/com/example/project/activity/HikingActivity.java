ef84823bb1c7d28ce42e1045a21a8be952562b23
Katie <katie.robinson@utah.edu>
2019-09-04 11:48:58 -0600

    Removed all the crazy location stuff from the hiking activity after discussion with Erik

--- Project/app/src/main/java/com/example/project/activity/HikingActivity.java
+++ Project/app/src/main/java/com/example/project/activity/HikingActivity.java
@@ -1,103 +1,27 @@
 package com.example.project.activity;
 
-import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.project.R;
 
-import java.util.Locale;
-
 public class HikingActivity extends AppCompatActivity {
-
-    private final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 111;
-    private String latitude;
-    private String longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiking);
 
-        getLocation();
-
         // Open the Google Maps app and search for nearby hiking trails
-        Uri gmmIntentUri = Uri.parse("geo:" + latitude + "," + longitude + "-?q=hiking");
+        Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + "hiking");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
     }
-
-    public void getLocation() {
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-        criteria.setPowerRequirement(Criteria.POWER_LOW);
-        criteria.setAltitudeRequired(false);
-        criteria.setBearingRequired(false);
-        criteria.setSpeedRequired(false);
-        criteria.setCostAllowed(true);
-        criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
-        criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
-
-        final LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        final Looper looper = null;
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
-        } else {
-            locationManager.requestSingleUpdate(criteria, locationListener, looper);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
-                } else {
-                    Toast.makeText(getApplicationContext(), "GetFit() cannot access your location.", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    final LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            Log.d("Location Changed", location.toString());
-            latitude = String.format(Locale.US, "%.4f", location.getLatitude());
-            longitude = String.format(Locale.US, "%.4f", location.getLongitude());
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Log.d("Status Changed", String.valueOf(status));
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            Log.d("Provider Enabled", provider);
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Log.d("Provider Disabled", provider);
-        }
-    };
 
 }
