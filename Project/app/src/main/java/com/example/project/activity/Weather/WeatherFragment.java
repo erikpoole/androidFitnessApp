17d24a3f729859e173a0a733bab179d3c199c9bc
Erik Poole <Mahkumazahn@gmail.com>
2019-09-09 11:05:23 -0600

    Weather Changes

--- Project/app/src/main/java/com/example/project/activity/Weather/WeatherFragment.java
+++ Project/app/src/main/java/com/example/project/activity/Weather/WeatherFragment.java
@@ -1,19 +1,12 @@
 package com.example.project.activity.Weather;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 import com.example.project.AssetHandlers;
