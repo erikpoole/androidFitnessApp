4aa7aa184c006f4e697b6905112abe7fb819052b
Erik Poole <Mahkumazahn@gmail.com>
2019-09-09 14:04:40 -0600

    CalorieActivity underway

--- Project/app/src/main/java/com/example/project/activity/bio/CalorieActivity.java
+++ Project/app/src/main/java/com/example/project/activity/bio/CalorieActivity.java
@@ -2,34 +2,53 @@ package com.example.project.activity.bio;
 
 import android.os.Bundle;
 
+import com.example.project.database.UserProfile;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.project.R;
 
 public class CalorieActivity extends AppCompatActivity {
 
+    private final int MALE_CALORIES = 2500;
+    private final int FEMALE_CALORIES = 2000;
+    private final int MALE_ACTIVITY_MODIFIER = 500;
+    private final int FEMALE_ACTIVITY_MODIFIER = 400;
+    private final int CALORIES_PER_POUND = 500;
+
+    private int activityLevel;
+    private int weightGoal;
+    private boolean isMale;
+
+    private UserProfile user;
+
+    private TextView caloriesView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calorie);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setTitle("Calorie Calculator");
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        user = new UserProfile(getApplicationContext());
+        Log.d("DATA:", user.getActiveState());
     }
 
+    private int calculateCalories() {
+        return (isMale ? MALE_CALORIES: FEMALE_CALORIES) +
+                weightGoal * CALORIES_PER_POUND +
+                activityLevel * (isMale ? MALE_ACTIVITY_MODIFIER: FEMALE_ACTIVITY_MODIFIER);
+    }
+
+    private int getActivityLevelInt() {
+        String activityLevelString = user.getActiveState();
+//        if (get )
+    }
 }
