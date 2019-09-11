4091200ed62600bfa342175056ce7de52fad7f3c
Erik Poole <Mahkumazahn@gmail.com>
2019-09-11 12:43:45 -0600

    CalorieActivity Interactive

--- Project/app/src/main/java/com/example/project/activity/bio/CalorieActivity.java
+++ Project/app/src/main/java/com/example/project/activity/bio/CalorieActivity.java
@@ -1,12 +1,9 @@
 package com.example.project.activity.bio;
 
 import android.os.Bundle;
-import android.util.Log;
-<<<<<<< HEAD
 import android.view.View;
-import android.widget.Switch;
-=======
->>>>>>> 19b73413cad0a5e502465dee7b3ceb5f745e78ce
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,21 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.project.R;
 import com.example.project.database.UserProfile;
 
-public class CalorieActivity extends AppCompatActivity {
+public class CalorieActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
     private final int MALE_CALORIES = 2500;
-    private final int FEMALE_CALORIES = 2000;
     private final int MALE_ACTIVITY_MODIFIER = 500;
-    private final int FEMALE_ACTIVITY_MODIFIER = 400;
-    private final int CALORIES_PER_POUND = 500;
 
-    private int activityLevel;
-    private int weightGoal;
-    private boolean isMale;
+    private final int FEMALE_CALORIES = 2000;
+    private final int FEMALE_ACTIVITY_MODIFIER = 400;
+
+    private final int NONBINARY_CALORIES = (MALE_CALORIES + FEMALE_CALORIES) / 2;
+    private final int NONBINARY_ACTIVITY_MODIFER = (MALE_ACTIVITY_MODIFIER + FEMALE_ACTIVITY_MODIFIER) / 2;
+
+    private final int CALORIES_PER_POUND = 500;
 
     private UserProfile user;
 
-    private TextView caloriesView;
+    private TextView caloriesText;
+    private TextView goalText;
+    private TextView activityText;
+    private SeekBar goalSeekBar;
+    private SeekBar activitySeekBar;
+    private Button resetButton;
+    private Button saveButton;
 
 
     @Override
@@ -37,30 +41,89 @@ public class CalorieActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calorie);
 
         user = new UserProfile(getApplicationContext());
-        activityLevel = getActivityLevelInt();
-//        weightGoal = user.getGoal();
-        weightGoal = 0;
-        isMale = user.getSex() == "male" ? true : false;
+
+        caloriesText = findViewById(R.id.calorieText);
+        caloriesText.setText(calculateCalories());
+        goalText = findViewById(R.id.goalValueText);
+        goalText.setText(user.getGoal() + "lbs");
+        activityText = findViewById(R.id.activityValueText);
+        activityText.setText(user.getActiveState());
+
+        goalSeekBar = findViewById(R.id.goalSeekBar);
+        goalSeekBar.setOnSeekBarChangeListener(this);
+
+        activitySeekBar = findViewById(R.id.activitySeekBar);
+        activitySeekBar.setOnSeekBarChangeListener(this);
+
+        resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(this);
+
+        saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(this);
     }
 
-    private int calculateCalories() {
-        return (isMale ? MALE_CALORIES: FEMALE_CALORIES) +
-                weightGoal * CALORIES_PER_POUND +
-                activityLevel * (isMale ? MALE_ACTIVITY_MODIFIER: FEMALE_ACTIVITY_MODIFIER);
-    }
 
-    //TODO what if null?
-    private int getActivityLevelInt() {
-        String activityLevelString = user.getActiveState();
-        switch (activityLevelString) {
-            case "active":
-                return 1;
-            case "standard":
-                return 0;
-            case "sedentary":
-                return -1;
+
+    private String calculateCalories() {
+        String sex = user.getSex();
+        switch (sex) {
+            case "male":
+                return (MALE_CALORIES +
+                        user.getGoal() * CALORIES_PER_POUND +
+                        getActivityLevelInt() * MALE_ACTIVITY_MODIFIER);
+            case "female:":
+                return (FEMALE_CALORIES +
+                        user.getGoal() * CALORIES_PER_POUND +
+                        getActivityLevelInt() * FEMALE_ACTIVITY_MODIFIER);
             default:
-                return 0;
+                return (NONBINARY_CALORIES +
+                        user.getGoal() * CALORIES_PER_POUND +
+                        getActivityLevelInt() * NONBINARY_ACTIVITY_MODIFER);
         }
+    }
+
+    private String getActivityText(int value) {
+        switch (value) {
+            case 0:
+                return "Low Activity";
+            case 1:
+                return "Moderate Activity";
+            case 2:
+                return "High Activity";
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch (seekBar.getId()) {
+            case R.id.goalSeekBar:
+                int pounds = progress - 2;
+                if (pounds < 0) {
+                    goalText.setText("Lose " + pounds + " Pounds");
+                } else if (pounds > 0) {
+                    goalText.setText("Gain " + pounds + " Pounds");
+                } else {
+                    goalText.setText("Maintain Weight");
+                }
+                break;
+            case R.id.activitySeekBar:
+                activityText.setText(getActivityText(progress));
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        return;
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        return;
     }
 }
