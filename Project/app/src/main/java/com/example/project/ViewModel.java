edd1adacef803be843b70a993c5c5cfa292acf9a
Erik Poole <Mahkumazahn@gmail.com>
2019-10-02 09:43:43 -0600

    ViewModel added

---
+++ Project/app/src/main/java/com/example/project/ViewModel.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ViewModel extends AndroidViewModel {
+
+    private Repository repository;
+    private LiveData<List<UserDBEntity>> userAllWordsTemp;
+
+    public ViewModel(@NonNull Application application) {
+        super(application);
+        repository = new Repository(application);
+        userAllWordsTemp = repository.getAllWords();
+
+    }
+
+    LiveData<List<UserDBEntity>> getAllUserDB() {return userAllWordsTemp;}
+
+    public void insert(UserDBEntity userDBEntity) {repository.insert(userDBEntity);}
+}
